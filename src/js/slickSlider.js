c8f5f7d8892c2af0344755809eaa4a657fa39d3d
m.gromov <cycyrbita@mail.ru>
2024-05-23 17:58:44 +0300

    return form, liveTime for cookie, remove autoPlay

--- src/js/slickSlider.js
+++ src/js/slickSlider.js
@@ -3,20 +3,12 @@ import 'slick-carousel'
 
 const autoplay = {
     rows: 0,
-    infinite: true,
-    adaptiveHeight: true,
+    swipeToSlide: true,
+    infinite: false,
     slidesToScroll: 1,
     slidesToShow: 4,
     arrows: false,
-    variableWidth: true,
     dots: false,
-    autoplay: true,
-    autoplaySpeed: 0,
-    speed: 6000,
-    cssEase: 'linear',
-    waitForAnimate: false,
-    pauseOnFocus: false,
-    pauseOnHover: false
 }
 
 $('.result__list_1').slick(autoplay)
@@ -24,8 +16,8 @@ $('.result__list_2').slick(autoplay)
 
 $('.lessons__list').slick({
     rows: 0,
-    infinite: true,
-    adaptiveHeight: true,
+    swipeToSlide: true,
+    infinite: false,
     slidesToScroll: 1,
     slidesToShow: 3,
     variableWidth: true,
@@ -45,8 +37,8 @@ $('.lessons__list').slick({
 
 $('.comments__list').slick({
     rows: 0,
-    infinite: true,
-    adaptiveHeight: true,
+    swipeToSlide: true,
+    infinite: false,
     slidesToScroll: 1,
     slidesToShow: 1,
     dots: true,
@@ -56,8 +48,8 @@ $('.comments__list').slick({
 
 $('.expert__list').slick({
     rows: 0,
-    infinite: true,
-    adaptiveHeight: true,
+    swipeToSlide: true,
+    infinite: false,
     slidesToScroll: 1,
     slidesToShow: 2,
     dots: false,
